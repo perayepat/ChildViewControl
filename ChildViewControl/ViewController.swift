c52ee5d92d9f9791f02ac6ac4f405c4c9fcb6fec
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2022-09-27 20:15:40 +0200

    Initial Commit

---
+++ ChildViewControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChildViewControl
+//
+//  Created by Pat on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
